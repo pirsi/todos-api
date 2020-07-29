d525ba89d2fecd56e2bd904961503f645cd2cd1f
mp <mploureno@gmail.com>
2020-07-29 16:54:48 -0300

    Write model specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #ensure associations
+  it { should belong_to(:todo) }
+  #presence validation
+  it { should validate_presence_of(:name) }
 end
